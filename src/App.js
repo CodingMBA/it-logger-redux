ef7b7377f320e8cb6640a3efd76c75db62472deb
Andrew Smith <codingmba@gmail.com>
2019-06-10 21:14:11 -0400

    App setup with json-server, concurrently, materialize, and material icons.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+
   return (
     <div className='App'>
       <h1>Welcome to the IT Logger!!</h1>
     </div>
   );
-}
+};
 
 export default App;
